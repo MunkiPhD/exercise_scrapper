9c818339cdfc2e8f57f07ae74e6b3e6a133b0b37
MunkiPhD <munkiphd@gmail.com>
2014-05-19 17:31:22 -0400

    modified to handle variable length in exercise details

--- scrapper.rb
+++ scrapper.rb
@@ -3,12 +3,13 @@ require 'nokogiri'
 require 'open-uri'
 require 'mechanize'
 
-puts 'hello!'
+puts 'script starting'
+puts '--------------------------------------------'
 #url = 'http://www.bodybuilding.com/exercises/list/index/selected/a'
 #doc = Nokogiri::HTML(open(url))
-file_path = '/home/munkiphd/development/exercise_scrapper/test_page.html'
-doc = Nokogiri::HTML(File.open(file_path))
-puts doc.at_css('title').text
+#file_path = '/home/munkiphd/development/exercise_scrapper/test_page.html'
+#doc = Nokogiri::HTML(File.open(file_path))
+#puts doc.at_css('title').text
 
 class ExerciseParser
 	def initialize(doc)
@@ -28,26 +29,52 @@ class ExerciseParser
 	def self.exercise_info(exercise_page)
 		info = {}
 		info[:name] = exercise_page.search('h1')[0].text.strip
-		# 0 = Type
-		# 1 = Main Muscle
-		# 2 = Other Muscles
-		# 3 = Equipment
-		# 4 = Mechanics type
-		# 5 = Equipment
-		# 6 = Competence level
-		# 7 = is a sport
-		# 8 = force type
 		details = exercise_page.search('#exerciseDetails a').map(&:text)
+		if details.length > 8
+			return parse_long_info(info, details)
+		else
+			return parse_short_info(info, details)
+		end
+	end
+
+	private
+	def self.parse_short_info(info, details)
+		#	0 = Type
+		# 1 = Main Muscle
+		# 2 = Equipment
+		# 3 = Mechanics type
+		# 4 = Equipment
+		# 5 = Competence level
+		# 6 = is a sport
+		# 7 = force type
 		info[:main_muscle] = details[1]
+		info[:equipment] = details[2]
 		info[:mechanics] = details[3]
 		info[:level] = details[4]
 		info[:force] = details[6]
 		info
 	end
+
+	def self.parse_long_info(info, details)
+		# 0 = Type
+		# 1 = Main Muscle
+		# 2 - length - 7 = Other Muscles 
+		# length - 7 = Equipment
+		# length - 6 = Mechanics type
+		# length - 5 = Equipment
+		# length - 4 = Competence level
+		# length - 3 = is a sport
+		# length - 2 = force type
+		length = details.length
+		info[:main_muscle] = details[1]
+		info[:equipment] = details[length - 6]
+		info[:mechanics] = details[length - 5]
+		info[:level] = details[length - 4]
+		info[:force] = details[length - 2]
+		info
+	end
 end
 
-#parser = ExerciseParser.new(doc)
-#parser.pages
 
 # we start off with the 0-9 because there are only two exercises there, and the # in the links throws the parsing off
 url = 'http://www.bodybuilding.com/exercises/list/index/selected/0-9'
@@ -56,23 +83,19 @@ listings = agent.get(url)
 listings.search('#pager a').each do |listing|
 	# each individual exercise by letter
 	exercises_letter_list = agent.click(listing)
+
+	# this iterates over all the exercises on the page
 	exercises_letter_list.search('h3 a').each do |exercise_listing|
-		puts "looking at: #{exercise_listing.text}"
+		puts "looking at: #{exercise_listing.text.strip}"
 		unless exercise_listing.text.include?("View All")
 			exercise_page = agent.click(exercise_listing)
-
 			info = ExerciseParser.exercise_info(exercise_page)
 			puts info
-			#exercise_name = exercise_page.search('h1')[0].text.strip
-			#puts "Exercise: '#{exercise_name}'"
-
-			#results = exercise_page.search('#exerciseDetails a').map(&:text)
-
-			#puts results
 			break
 		end
 	end
 	break
 end
 
-puts 'the end'
+puts '--------------------------------------------'
+puts 'script ended!'
